83b7e9ec12e8b4b2e90fa4d71ec8a37d361b6f5c
Justin Schellenberg <jrschellenberg@gmail.com>
2020-06-25 14:05:57 -0500

    :bug: Update Game for Checking previous Guesses

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -4,22 +4,14 @@ from phrasehunter.phrase import Phrase
 
 class Game:
     def __init__(self):
+        self._player_guesses = []
         self._lives = 5
         self.phrase = Phrase()
 
-    def start_game(self):
-        while self.is_still_alive():
-            print(self.get_phrase())
-            print(f'You have {self._lives} lives remaining')
-            self.guess_character()
-            if self.phrase.is_phrase_guessed():
-                break
-        if self.is_still_alive():
-            print("You won!")
-        else:
-            print("Sorry, you lost :(")
+    def _is_still_alive(self):
+        return 0 < self._lives
 
-    def guess_character(self):
+    def _guess_character(self):
         while True:
             try:
                 player_guess = input("Please guess a letter:  ")
@@ -27,15 +19,24 @@ class Game:
                     raise ValueError("Please enter 1 character only!")
                 if re.match(r'[A-z]', player_guess) is None:
                     raise ValueError("Need to guess an english alphabet uppercase, or lowercase Letter!")
+                if player_guess.lower() in self._player_guesses:
+                    raise ValueError(f"You have already guessed {player_guess}, Please guess another Letter!")
                 break
             except ValueError as e:
                 print(f'\n{e}\n')
         keep_life = self.phrase.player_guess(player_guess)
+        self._player_guesses.append(player_guess.lower())
         if not keep_life:
             self._lives -= 1
 
-    def is_still_alive(self):
-        return 0 < self._lives
-
-    def get_phrase(self):
-        return str(self.phrase)
+    def start_game(self):
+        while self._is_still_alive():
+            print(str(self.phrase))
+            print(f'You have {self._lives} lives remaining')
+            self._guess_character()
+            if self.phrase.is_phrase_guessed():
+                break
+        if self._is_still_alive():
+            print(f"You won! while maintaining {self._lives} lives! Congrats!")
+        else:
+            print("Sorry, you lost :(")
